4f9bff1e9eaced3e6bd87e542befcd62c69a3c57
NolanCrochot <crochot.nolan@gmail.com>
2016-06-18 03:24:20 +0200

    Création du portfolio et section Réalisations (portfolio)
    Un portfolio personnel codé en JS, CSS et HTML (ce défi était
    sympathique,  bien que la correction des petits bugs Jquery furent
    rudement chronophages !) + intégration de la section "Réalisations"

---
+++ js/portfolio.js
@@ -0,0 +1,68 @@
+$( document ).ready(function() {
+ 
+    $( '.port-item' ).hover(function() {
+        $( this ).addClass( "item-hover" );  
+    }, function() {
+        $( this ).removeClass( "item-hover" );   
+    });
+
+    $( '.cat-all' ).click(function() {
+        $( '.itemweb' ).removeClass( "portitem-hiden" );
+        $( '.itemgra' ).removeClass( "portitem-hiden" );
+        $( '.itemdev' ).removeClass( "portitem-hiden" );
+        $( '.cat-all' ).addClass( "cat-selected" );
+        $( '.cat-web' ).removeClass( "cat-selected" );
+        $( '.cat-gra' ).removeClass( "cat-selected" );
+        $( '.cat-dev' ).removeClass( "cat-selected" );
+        setTimeout(function() {
+            $( '.itemgra' ).removeClass( "portitem-transition" ); 
+            $( '.itemweb' ).removeClass( "portitem-transition" );
+            $( '.itemdev' ).removeClass( "portitem-transition" );
+        }, 250);
+    });
+    $( '.cat-web' ).click(function() {
+        $( '.itemweb' ).removeClass( "portitem-hiden" );
+        $( '.itemgra' ).addClass( "portitem-transition" );
+        $( '.itemdev' ).addClass( "portitem-transition" );
+        $( '.cat-web' ).addClass( "cat-selected" );
+        $( '.cat-gra' ).removeClass( "cat-selected" );
+        $( '.cat-dev' ).removeClass( "cat-selected" );
+        $( '.cat-all' ).removeClass( "cat-selected" );
+        setTimeout(function() {
+            $( '.itemgra' ).addClass( "portitem-hiden" ); 
+            $( '.itemdev' ).addClass( "portitem-hiden" ); 
+            $( '.itemweb' ).removeClass( "portitem-transition" );
+        }, 250);
+    });
+    $( '.cat-gra' ).click(function() {
+        $( '.itemweb' ).addClass( "portitem-transition" );
+        $( '.itemgra' ).removeClass( "portitem-hiden" );
+        $( '.itemdev' ).addClass( "portitem-transition" );
+        $( '.cat-web' ).removeClass( "cat-selected" );
+        $( '.cat-gra' ).addClass( "cat-selected" );
+        $( '.cat-dev' ).removeClass( "cat-selected" );
+        $( '.cat-all' ).removeClass( "cat-selected" );
+        setTimeout(function() {
+            $( '.itemweb' ).addClass( "portitem-hiden" );  
+            $( '.itemdev' ).addClass( "portitem-hiden" ); 
+            $( '.itemgra' ).removeClass( "portitem-transition" );
+        }, 250);
+    });
+    $( '.cat-dev' ).click(function() {
+        $( '.itemweb' ).addClass( "portitem-transition" );
+        $( '.itemgra' ).addClass( "portitem-transition" );
+        $( '.itemdev' ).removeClass( "portitem-hiden" );
+        $( '.cat-web' ).removeClass( "cat-selected" );
+        $( '.cat-gra' ).removeClass( "cat-selected" );
+        $( '.cat-dev' ).addClass( "cat-selected" );
+        $( '.cat-all' ).removeClass( "cat-selected" );
+        setTimeout(function() {
+            $( '.itemweb' ).addClass( "portitem-hiden" ); 
+            $( '.itemgra' ).addClass( "portitem-hiden" ); 
+            $( '.itemdev' ).removeClass( "portitem-transition" );
+        }, 250);
+    });
+    
+});
+
+
